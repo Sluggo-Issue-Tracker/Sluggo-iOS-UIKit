b31ef7e10e6919612e628cdb470a74d9b19eddbe
Isaac Trimble-Pederson <wtpisaac@icloud.com>
2021-04-17 14:48:53 -0700

    Attempted to fix problems and realizing this is a bad approach
    This should be done with a custom presentation, not with a floating
    view. Whoops. I'll try to investigate how to do this later.

---
+++ Sluggo/Models/SidebarData.swift
@@ -0,0 +1,8 @@
+//
+//  SidebarData.swift
+//  Sluggo
+//
+//  Created by Isaac Trimble-Pederson on 4/17/21.
+//
+
+import Foundation
---
+++ Sluggo/View Controllers/RootViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RootViewController.swift
+//  Sluggo
+//
+//  Created by Isaac Trimble-Pederson on 4/17/21.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
