17933d2513f70ef2659739ce4e4b696cd3133ab6
Isaac Trimble-Pederson <wtpisaac@icloud.com>
2021-04-18 17:16:40 -0700

    Working but potentially inefficient approach

--- Sluggo/Models/User.swift
+++ Sluggo/Models/User.swift
@@ -14,4 +14,42 @@ struct UserRecord: Codable {
     var first_name: String?
     var last_name: String?
     var username: String
+    
+    init(id: Int, email: String, first_name: String?, last_name: String?, username: String) {
+        self.id = id
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+    }
+    
+    init?(fromJSONString: String) { // Initializer for data from JSON
+        // Get data
+        guard let jsonData = fromJSONString.data(using: .utf8) else { return nil }
+        
+        // Attempt decoding
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        
+        guard let userRecord = try? decoder.decode(UserRecord.self, from: jsonData) else { return nil }
+        
+        // Initialize using standard initializer
+        self.init(id: userRecord.id, email: userRecord.email, first_name: userRecord.first_name, last_name: userRecord.last_name, username: userRecord.username)
+    }
+    
+    var jsonString: String? {
+        get {
+            // obtain an encoder
+            
+            let encoder = JSONEncoder()
+            encoder.dateEncodingStrategy = .iso8601
+            encoder.outputFormatting = .prettyPrinted
+            
+            // Attempt encoding
+            guard let jsonData = try? encoder.encode(self) else { return nil }
+            
+            // Attempt stringifying the data, this is failable, which is fine since property is optional.
+            return String(data: jsonData, encoding: .utf8)
+        }
+    }
 }
---
+++ SluggoTests/UserTests.swift
@@ -0,0 +1,21 @@
+//
+//  UserTests.swift
+//  SluggoTests
+//
+//  Created by Isaac Trimble-Pederson on 4/18/21.
+//
+
+import XCTest
+@testable import Sluggo
+
+class UserTests: XCTestCase {
+    let testWorkingJson = """
+    {
+        
+    }
+    """
+    
+    func testUserDoesDeserialize() {
+        
+    }
+}
