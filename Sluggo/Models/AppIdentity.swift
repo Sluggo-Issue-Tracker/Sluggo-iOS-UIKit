a744280f9fd0e5c496c1d31e07221e843e3562e3
Isaac Trimble-Pederson <wtpisaac@icloud.com>
2021-04-23 12:54:09 -0700

    Remove unnecessary computed properties and convert AppIdentity to struct

--- Sluggo/Models/AppIdentity.swift
+++ Sluggo/Models/AppIdentity.swift
@@ -7,29 +7,14 @@
 
 import Foundation
 
-class AppIdentity {
-    private var _authenticatedUser: UserRecord
-    private var _team: TeamRecord
-    private var _token: String
+struct AppIdentity {
+    var authenticatedUser: UserRecord
+    var team: TeamRecord
+    var token: String
     
-    var authenticatedUser: UserRecord {
-        get { return _authenticatedUser }
-        set { _authenticatedUser = newValue }
-    }
-
-    var team: TeamRecord {
-        get { return _team }
-        set { _team = newValue }
-    }
-    
-    var token: String {
-        get { return _token }
-        set { _token = newValue }
-    }
-    
-    init(_ authenticatedUser: UserRecord, _ team: TeamRecord, _ token: String) {
-        self._authenticatedUser = authenticatedUser
-        self._team = team
-        self._token = token
+    init(authenticatedUser: UserRecord, team: TeamRecord, token: String) {
+        self.authenticatedUser = authenticatedUser
+        self.team = team
+        self.token = token
     }
 }
