a62681eb4eb0fa556e18d9cc1d298b5b2a67fcbf
Samuel Schmidt <tdimhcsleumas@gmail.com>
2021-04-30 01:05:22 -0700

    initial design. pagination. reloading. infinite scrolling

--- Sluggo/Components/TicketTableViewCell.swift
+++ Sluggo/Components/TicketTableViewCell.swift
@@ -11,10 +11,6 @@ class TicketTableViewCell: UITableViewCell {
     @IBOutlet var containerView: UIView! {
         didSet {
             containerView.layer.cornerRadius = 5
-            containerView.layer.shadowColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
-            containerView.layer.shadowOpacity = 1
-            containerView.layer.shadowOffset = CGSize(width: 3, height: 3)
-            containerView.layer.shadowRadius = 5
         }
     }
     
@@ -25,4 +21,18 @@ class TicketTableViewCell: UITableViewCell {
     }
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var assignedLabel: UILabel!
+    
+    func loadFromTicketRecord(ticket: TicketRecord) {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        formatter.locale = Locale(identifier: "en_US")
+        
+        titleLabel.text = "\(ticket.ticket_number) | \(ticket.title.capitalized)"
+        if let date = ticket.due_date {
+            assignedLabel.text = formatter.string(from: date)
+        } else {
+            assignedLabel.text = ""
+        }
+    }
 }
