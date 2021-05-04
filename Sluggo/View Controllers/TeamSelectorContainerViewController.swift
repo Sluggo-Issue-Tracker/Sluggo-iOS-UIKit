26b169f574169524a893ccb14fcfbd1c67154880
Samuel Schmidt <tdimhcsleumas@gmail.com>
2021-05-04 01:19:34 -0700

    wrap team list in nav controller

--- Sluggo/View Controllers/TeamSelectorContainerViewController.swift
+++ Sluggo/View Controllers/TeamSelectorContainerViewController.swift
@@ -31,17 +31,28 @@ class TeamSelectorContainerViewController: UIViewController {
     
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-        if let vc = segue.destination as? TeamTableViewController {
-            vc.identity = self.identity
-            vc.completion = { team in
-                self.identity.team = team
-                self.dismiss(animated: true, completion: self.completion)
-            }
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//        if let vc = segue.destination as? TeamTableViewController {
+//            vc.identity = self.identity
+//            vc.completion = { team in
+//                self.identity.team = team
+//                self.dismiss(animated: true, completion: self.completion)
+//            }
+//        }
+//    }
+    
+    @IBSegueAction func launchTeamSelect(_ coder: NSCoder) -> TeamTableViewController? {
+        let vc = TeamTableViewController(coder: coder)
+        vc?.identity = self.identity
+        vc?.completion = { team in
+            self.identity.team = team
+            self.dismiss(animated: true, completion: self.completion)
         }
+        
+        return vc
     }
     
 
