5d5af28953edb11adc07f4024119e03f178b192f
tdimhcsleumas <56523092+tdimhcsleumas@users.noreply.github.com>
2021-05-05 18:31:34 -0700

    Revert "Smarti84/ticket creation"

--- Sluggo/Models/Codables/PaginatedList.swift
+++ Sluggo/Models/Codables/PaginatedList.swift
@@ -6,12 +6,11 @@
 //
 
 import Foundation
-import NullCodable
 
 struct PaginatedList<T : Codable> : Codable {
     var count: Int
-    @NullCodable var next: String?
-    @NullCodable var previous: String?
+    var next: String?
+    var previous: String?
     let results: [T]
     
 }
--- Sluggo/Models/Codables/User.swift
+++ Sluggo/Models/Codables/User.swift
@@ -6,20 +6,20 @@
 //
 
 import Foundation
-import NullCodable
+
 
 struct UserRecord: Codable {
     var id: Int
     var email: String
-    @NullCodable var first_name: String?
-    @NullCodable var last_name: String?
+    var first_name: String?
+    var last_name: String?
     var username: String
 }
 
 struct AuthRecord: Codable {
     var pk: Int
     var email: String
-    @NullCodable var first_name: String?
-    @NullCodable var last_name: String?
+    var first_name: String?
+    var last_name: String?
     var username: String
 }
